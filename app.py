debea9128c76621c87d552c6200a957bdbbdc8fa
Seth McCullough <seth@endevrs.dev>
2022-03-28 18:25:45 -0400

    Create and write to bed.dep output file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import numpy as np
 import rasterio
 
 # Open the GeoTIFF using the GDAL format driver
@@ -7,3 +8,8 @@ height = dataset.height
 
 # Read the data from the GeoTIFF
 data = dataset.read(1).astype('float64')
+
+# Create output bathymetry file
+with open("output/bed.dep", "w") as f:
+    for x in np.nditer(data):
+        f.write(f"{x} ")
