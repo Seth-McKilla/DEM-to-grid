9d17534f4d13628ab25d45623165b6ccbdfb280b
Seth McCullough <seth@endevrs.dev>
2022-04-01 12:23:56 -0400

    Print grid height and width

--- index.py
+++ index.py
@@ -2,14 +2,17 @@ import numpy as np
 import rasterio
 
 # Open the GeoTIFF using the GDAL format driver
-dataset = rasterio.open("./data/topo.tif")
+dataset = rasterio.open('./data/topo.tif')
 width = dataset.width
 height = dataset.height
+
+# Plot grid width and height
+print(f'W: {width} H: {height}')
 
 # Read the data from the GeoTIFF
 data = dataset.read(1).astype('float64')
 
 # Create output bathymetry file
-with open("output/bed.dep", "w") as f:
+with open('output/bed.dep', 'w') as f:
     for x in np.nditer(data):
-        f.write(f"{x} ")
+        f.write(f'{x} ')
