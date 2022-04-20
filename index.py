0539a0bdd873c05619ab8cfdbbbe782621585bf9
Seth McCullough <seth@endevrs.dev>
2022-04-20 19:59:49 -0400

    Convert to meters and filter out nodata

--- index.py
+++ index.py
@@ -2,24 +2,25 @@ import numpy as np
 import rasterio
 
 # Open the GeoTIFF using the GDAL format driver
-dataset = rasterio.open('./data/topo.tif')
+dataset = rasterio.open("./data/topo.tif")
 width = dataset.width
 height = dataset.height
 
-# Plot grid width and height
-print(f'W: {width} H: {height}')
-
 # Read the data from the GeoTIFF
-data = dataset.read(1).astype('float64')
+data = dataset.read(1).astype("float")
+data = np.round(data * 0.3048, 6)  # Convert to meters and set precision
+data[(data < -1e10) | (data > 2e10)] = 0  # Set nodata values to 0
 
 # Create output bathymetry file
 counter = 0
 
-with open('output/bathy.dep', 'w') as f:
+with open("output/bathy.dep", "w") as f:
+    print("Writing output bathymetry file...")
     for x in np.nditer(data):
-        if (counter == width):
-            f.write('\n')
+        if counter == width:
+            f.write("\n")
             counter = 0
-            
-        f.write(f'{x} ')
+
+        f.write(f"{x} ")
         counter += 1
+    print("Done!")
