90e04e570e7fa664f3598d304f8d490b38a95c46
Seth McCullough <seth@endevrs.dev>
2022-04-21 13:26:40 -0400

    Filter out nodata values

--- index.py
+++ index.py
@@ -9,7 +9,6 @@ height = dataset.height
 # Read the data from the GeoTIFF
 data = dataset.read(1).astype("float")
 data = np.round(data, 6)  # Convert to meters and set precision
-data[(data < -1e10) | (data > 1e10)] = 0  # Set nodata values to 0
 
 # Create output bathymetry file
 counter = 0
@@ -24,3 +23,8 @@ with open("output/bathy.dep", "w") as f:
         f.write(f"{x} ")
         counter += 1
     print("Done!")
+
+bathy = np.loadtxt("output/bathy.dep")
+bathy = bathy[:, ~np.all(bathy > 1e5, axis = 0)]
+bathy = bathy[~np.all(bathy > 1e5, axis = 1), :]
+np.savetxt("output/bathy.dep", bathy, fmt = "%f")
